5910e0f65b727fe042af4cb893a1e660937a8994
Adrian <aracosta2810@gmail.com>
2022-12-11 14:38:05 -0500

    Functions added: -Basic login -Junior form - Forms validation -Code refactored

---
+++ components/RegisterForms/IsJuniorModal.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+const IsJuniorModal = ({ setIsJunior, handleNext }) => {
+  const [radioValue, setRadioValue] = useState(0);
+
+  const handleSubmit = () => {
+    setIsJunior(radioValue);
+    if (radioValue === 1) {
+      setIsJunior(1);
+      handleNext(4);
+    } else if (radioValue === 0) {
+      setIsJunior(0);
+      handleNext(0);
+    }
+  };
+  return (
+    <div
+      className="modal fade show"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex={-1}
+      aria-labelledby="staticBackdropLabel"
+      style={{ display: 'block', backgroundColor: 'rgba(255,255,255,0.8)' }}
+      aria-modal="true"
+      role="dialog"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content mt-5">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="staticBackdropLabel">
+              No se que poner aqui
+            </h1>
+          </div>
+          <div className="modal-body">
+            <div className="form-check">
+              <input
+                id="radio1"
+                className="form-check-input"
+                type="radio"
+                checked={radioValue === 0}
+                onChange={() => setRadioValue(0)}
+              />
+              <label className="form-check-label" htmlFor="radio1">
+                Global Game Jam
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                id="radio2"
+                className="form-check-input"
+                type="radio"
+                checked={radioValue === 1}
+                onChange={() => setRadioValue(1)}
+              />
+              <label className="form-check-label" htmlFor="radio2">
+                Global Game Jam Next
+              </label>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              onClick={handleSubmit}
+              type="button"
+              className="btn btn-primary GG_button"
+            >
+              Siguiente
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IsJuniorModal;
--- store/store.js
+++ store/store.js
@@ -1,101 +1,123 @@
-import { action, createStore, thunk } from "easy-peasy";
+import { action, createStore, thunk } from 'easy-peasy';
 
 const store = createStore({
-    team: {
-        name: '',
-        desc: ''
+  team: {
+    name: '',
+    desc: '',
+  },
+  addTeamInfo: action((state, payload) => {
+    state.team = payload;
+  }),
+  participants: [
+    {
+      id: '',
+      name: '',
+      institution: '',
+      phone: '',
+      email: '',
+      area: '',
+      ext: false,
+      gender: '',
     },
-    addTeamInfo: action((state, payload) => {
-        state.team = payload;
-    }),
-    participants: [
-        {
-            id: "",
-            name: "",
-            institution: "",
-            phone: "",
-            email: "",
-            area: "",
-            ext: false,
-            gender: ""
-        }
-    ],
-    cleanData: action((state) => {
-        state.team = {
-            name: '',
-            desc: '',
-        }
-        state.participants = []
-        for (let index = 0; index < 4; index++) {
-            state.participants.push({
-                id: "",
-                name: "",
-                institution: "",
-                phone: "",
-                email: "",
-                area: "",
-                ext: false,
-                gender: ""
-            })
+  ],
+  cleanData: action((state) => {
+    state.team = {
+      name: '',
+      desc: '',
+    };
+    state.participants = [];
+    for (let index = 0; index < 4; index++) {
+      state.participants.push({
+        id: '',
+        name: '',
+        institution: '1',
+        phone: '',
+        email: '',
+        area: '',
+        ext: false,
+        gender: 'masculino',
+      });
+    }
+  }),
+  setParticipant: action((state, payload) => {
+    const { index, data, key } = payload;
+    state.participants[index][key] = data;
+  }),
+  juniorParticipant: {
+    id: '',
+    name: '',
+    age: '',
+    institution: '',
+    //Responsible data
+    responsible: '',
+    responsibleId: '',
+    phone: '',
+    email: '',
+    ext: false,
+  },
+  setJuniorParticipant: action((state, payload) => {
+    const { data, key } = payload;
+    state.juniorParticipant[key] = data;
+  }),
 
-        }
-    }),
-    setParticipant: action((state, payload) => {
-        const { index, data, key } = payload
-        state.participants[index][key] = data
-    }),
-    sendData: action((state, payload) => {
-        console.log(JSON.stringify({ team: state.team, participants: state.participants }))
-        //TODO: send data
-        // const options = {
-        // 	method: "POST",
-        // 	url: "http://127.0.1:3000/api/reg/team",
-        // 	params: { "": "" },
-        // 	headers: { "Content-Type": "application/json" },
-        // 	data: {
-        // 		team: { name: "Las avispas", desc: "pican y no hacen miel" },
-        // 		participants: [
-        // 			{
-        // 				id: "99013110582",
-        // 				name: "Daniel Chaviano Perez",
-        // 				institution: "UCI",
-        // 				phone: "5356064362",
-        // 				email: "danielcp@estudiantes.uci.cu",
-        // 				area: "Fac-4",
-        // 				ext: false,
-        // 			},
-        // 			{
-        // 				id: "00051967686",
-        // 				name: "Fulanita Martinez Perez",
-        // 				institution: "CUJAE",
-        // 				phone: "5356064332",
-        // 				email: "fmartinez@estudiantes.cujae.cu",
-        // 				area: "Fac-electrica",
-        // 				ext: true,
-        // 			},
-        // 			{
-        // 				id: "00010110101",
-        // 				name: "LaQseBurla Alfaro Manso",
-        // 				institution: "Fajardo",
-        // 				phone: "5356064312",
-        // 				email: "michino@estudiantes.fcm.cu",
-        // 				area: "matasanos",
-        // 				ext: true,
-        // 			},
-        // 		],
-        // 	},
-        // };
+  sendData: action((state, payload) => {
+    //payload tendra 0 o 1. Si es 1 hay que mandar el junior, si no los otros
+    if (payload === 0) {
+      console.log(
+        JSON.stringify({ team: state.team, participants: state.participants }),
+      );
+    } else {
+      console.log(JSON.stringify({ participant: state.juniorParticipant }));
+    }
+    //TODO: send data
+    // const options = {
+    // 	method: "POST",
+    // 	url: "http://127.0.1:3000/api/reg/team",
+    // 	params: { "": "" },
+    // 	headers: { "Content-Type": "application/json" },
+    // 	data: {
+    // 		team: { name: "Las avispas", desc: "pican y no hacen miel" },
+    // 		participants: [
+    // 			{
+    // 				id: "99013110582",
+    // 				name: "Daniel Chaviano Perez",
+    // 				institution: "UCI",
+    // 				phone: "5356064362",
+    // 				email: "danielcp@estudiantes.uci.cu",
+    // 				area: "Fac-4",
+    // 				ext: false,
+    // 			},
+    // 			{
+    // 				id: "00051967686",
+    // 				name: "Fulanita Martinez Perez",
+    // 				institution: "CUJAE",
+    // 				phone: "5356064332",
+    // 				email: "fmartinez@estudiantes.cujae.cu",
+    // 				area: "Fac-electrica",
+    // 				ext: true,
+    // 			},
+    // 			{
+    // 				id: "00010110101",
+    // 				name: "LaQseBurla Alfaro Manso",
+    // 				institution: "Fajardo",
+    // 				phone: "5356064312",
+    // 				email: "michino@estudiantes.fcm.cu",
+    // 				area: "matasanos",
+    // 				ext: true,
+    // 			},
+    // 		],
+    // 	},
+    // };
 
-        // axios
-        // 	.request(options)
-        // 	.then(function (response) {
-        // 		console.log(response.data);
-        // 	})
-        // 	.catch(function (error) {
-        // 		console.error(error);
-        // 	});
-    }),
-
+    // axios
+    // 	.request(options)
+    // 	.then(function (response) {
+    // 		console.log(response.data);
+    // 	})
+    // 	.catch(function (error) {
+    // 		console.error(error);
+    // 	});
+  }),
 });
 
 export default store;
